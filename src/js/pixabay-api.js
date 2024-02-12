14ba5f7f8126c42f02902f10fef2401220ad9127
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2024-02-12 22:31:40 +0200

    'update'

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,24 @@
-import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from 'axios';
+import { renderPicture } from "./render-functions";
 
 const input = document.querySelector('.js-search-form');
-const picCont = document.querySelector('.gallery');
+export const picCont = document.querySelector('.gallery');
+const picContAdd = document.querySelector('.js_picture_load');
+const loadElem = document.querySelector('.js-loader');
+// picContAdd.addEventListener('click', )
+
+let currentPage = 1;
 
 
 input.addEventListener('submit', e =>{
-
   e.preventDefault();
-
   document.getElementById("loader").style.display = "flex";
-
   const name = e.target.elements.form.value.trim();
 
-if(name === ''){
+  if(name === ''){
   e.target.reset();
   picCont.innerHTML = "";
   document.getElementById("loader").style.display = "none";
@@ -24,6 +27,7 @@ if(name === ''){
 
   searchPicture(name).then(data => {
     document.getElementById("loader").style.display = "none";
+    showSearc();
     renderPicture(data);
     gallery.refresh();
   });
@@ -31,7 +35,7 @@ if(name === ''){
   picCont.innerHTML = "";
 });
 
-function searchPicture(pixabay){
+async function searchPicture(pixabay){
   const BASE_URL = 'https://pixabay.com/api/?';
     const PARAMS = new URLSearchParams({
       key: '42108829-54920070c40067c28d8228e80',
@@ -39,61 +43,29 @@ function searchPicture(pixabay){
       image_type:'photo',
       orientation:'horizontal',
       safesearch: true,
-
+      page: currentPage,
+      per_page: 3,
     });
     const url = BASE_URL + PARAMS;
-  return fetch(url).then(res => res.json()).catch(error => {
-    console.error('Error retrieving image link:', error);
-  });
+
+    try{
+      const res = await axios.get(url);
+      return res.data;
+    }catch(error){
+      console.error('Error retrieving image link:', error);
+    };
 }
 
-function pictureTemplate({webformatURL,largeImageURL,tags,likes,views,comments,downloads}){
-  return `<li class="gallery-item">
-  <a class="gallery-link" href="${largeImageURL}">
-    <img class="gallery-image" src="${webformatURL}" alt="${tags}">
-    </a>
-    <ul class="description">
-      <li class="description-item">
-        <h2>likes</h2>
-        <h3>${likes}</h3>
-      </li>
-      <li class="description-item">
-        <h2>views</h2>
-        <h3>${views}</h3>
-      </li>
-      <li class="description-item">
-        <h2>comments</h2>
-        <h3>${comments}</h3>
-      </li>
-      <li class="description-item">
-        <h2>downloads</h2>
-        <h3>${downloads}</h3>
-      </li>
-    </ul>
-</li> `; }
-
-function picturesTemplate(picture){
-
-  let  pictureProm = picture.hits.map(pictureTemplate).join('');
-
-  if(pictureProm.length === 0){
-    iziToast.show({
-      message: 'Sorry, there are no images matching your search query. Please try again!',
-      messageColor: '#FFFFFF',
-      backgroundColor: '#B51B1B',
-      position: 'topRight',
-    });
-    return;
-  }else{
-    return pictureProm ;
-  }
+function showSearc() {
+  loadElem.classList.add('hidden');
+  picContAdd.classList.remove('hidden');
 }
 
-function renderPicture(picture) {
-  const markup = picturesTemplate(picture);
-  if(markup.length === 0)return;
-  picCont.insertAdjacentHTML('beforeend', markup);
+function hideSearc() {
+  loadElem.classList.add('hidden');
+  picContAdd.classList.add('hidden');
 }
+
 
 const gallery = new SimpleLightbox('.gallery a', {captionDelay: 250, captionsData: 'alt'});
 
---
+++ src/js/render-functions.js
@@ -0,0 +1,50 @@
+import iziToast from "izitoast";
+import { picCont } from "./pixabay-api";
+
+function pictureTemplate({webformatURL,largeImageURL,tags,likes,views,comments,downloads}){
+  return `<li class="gallery-item">
+  <a class="gallery-link" href="${largeImageURL}">
+    <img class="gallery-image" src="${webformatURL}" alt="${tags}">
+    </a>
+    <ul class="description">
+      <li class="description-item">
+        <h2>likes</h2>
+        <h3>${likes}</h3>
+      </li>
+      <li class="description-item">
+        <h2>views</h2>
+        <h3>${views}</h3>
+      </li>
+      <li class="description-item">
+        <h2>comments</h2>
+        <h3>${comments}</h3>
+      </li>
+      <li class="description-item">
+        <h2>downloads</h2>
+        <h3>${downloads}</h3>
+      </li>
+    </ul>
+</li> `; }
+
+function picturesTemplate(picture){
+
+  let  pictureProm = picture.hits.map(pictureTemplate).join('');
+
+  if(pictureProm.length === 0){
+    iziToast.show({
+      message: 'Sorry, there are no images matching your search query. Please try again!',
+      messageColor: '#FFFFFF',
+      backgroundColor: '#B51B1B',
+      position: 'topRight',
+    });
+    return;
+  }else{
+    return pictureProm ;
+  }
+}
+
+export function renderPicture(picture) {
+  const markup = picturesTemplate(picture);
+  if(markup.length === 0)return;
+  picCont.insertAdjacentHTML('beforeend', markup);
+}
